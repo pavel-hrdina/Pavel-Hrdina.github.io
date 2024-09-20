886c9b8e3ebbbef76833d6bdacf29aea367b56ad
Pavel <93209179+Pavel-Hrdina@users.noreply.github.com>
2024-09-20 17:10:42 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   // change this to your own domain
-  site: "https://barebones.superwebthemes.com",
+  site: "https://Pavel-Hrdina.github.io",
   integrations: [tailwind(), sitemap()],
 });
